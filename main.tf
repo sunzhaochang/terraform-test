9017ebb94faacaa164b6c3f9ed1c4df0565b4728
sunzhaochang <sunzhaochang@baidu.com>
2019-10-17 11:27:13 +0800

    add backend

--- main.tf
+++ main.tf
@@ -4,3 +4,17 @@ resource "alicloud_vpc" "default" {
   name       = "${var.name}"
   cidr_block = "172.16.0.0/12"
 }
+
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "clouder"
+    workspaces {
+      name = "terraform-test"
+    }
+  }
+}
+
+data "alicloud_vpcs" "default" {
+  backend = "remote"
+}
